8dea1cf738c42e9cb3d05acb690e6bcdca333644
kikozai <djumvlad@yandex.ru>
2023-11-24 17:55:34 +0500

    pref: оптимизировал регистрацию

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:23675@localhost/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:23675@localhost/child'  
 db = SQLAlchemy(app)
 
 # Модель ребёнка   
 class Child(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(50))
-    secondName = db.Column(db.String(50))
-    surname = db.Column(db.String(50))
-    password = db.Column(db.String(50))
-    age = db.Column(db.Integer)
-    sciences = db.Column(db.String(200))
-    email = db.Column(db.String(100), unique=True)
+    firstName = db.Column(db.String(100), nullable=False)
+    secondName = db.Column(db.String(100))
+    surname = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    sciences = db.Column(db.String(100))
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     phoneNumber = db.Column(db.String(20))
 
+    def __init__(self, firstName, secondName, surname, age, sciences, password, email, phoneNumber):
+        self.firstName = firstName
+        self.secondName = secondName
+        self.surname = surname
+        self.age = age
+        self.sciences = sciences
+        self.password = password
+        self.email = email
+        self.phoneNumber = phoneNumber
+        
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
@@ -34,40 +44,45 @@ def login_post():
         return render_template("child.html")
     else:
         return render_template("login.html", email=email, password=password, error=True)
-
-@app.route("/register", methods=["GET"])
+# Register route
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        # Get the registration data from the request
+        first_name = request.form.get('firstName')
+        second_name = request.form.get('secondName')
+        surname = request.form.get('surname')
+        password = request.form.get('password')
+        age = request.form.get('age')
+        sciences = request.form.getlist('sciences')
+        email = request.form.get('email')
+        phone_number = request.form.get('phoneNumber')
 
-@app.route("/register", methods=["POST"])
-def register_post():
-    firstName = request.form.get("firstName")
-    secondName = request.form.get("secondName")
-    surname = request.form.get("surname")
-    password = request.form.get("password")
-    age = request.form.get("age")
-    sciences = request.form.getlist("sciences")
-    email = request.form.get("email")
-    phoneNumber = request.form.get("phoneNumber")
-    error = request.form.get("error")
-    existing_child = Child.query.filter_by(email=email).first()
-    if existing_child:
-        return render_template("register.html", firstName=firstName, secondName=secondName, surname=surname,
-                               password=password, age=age, sciences=sciences, email=email, phoneNumber=phoneNumber,
-                               error=True)
-    else:
-        new_child = Child(firstName=firstName, secondName=secondName, surname=surname, password=password, age=age,
-                          sciences=sciences, email=email, phoneNumber=phoneNumber)
-        db.session.add(new_child)
-        db.session.commit()
-        return render_template("child.html")
+        # Check if a child with the given email already exists
+        existing_child = Child.query.filter_by(email=email).first()
 
-@app.route("/child", methods=["GET"])
+        if existing_child:
+            # Child with the given email already exists
+            return render_template('register.html', error=True, firstName=first_name, secondName=second_name,
+                                   surname=surname, password=password, age=age, sciences=sciences,
+                                   email=email, phoneNumber=phone_number)
+        else:
+            # Create a new child object
+            new_child = Child(first_name, second_name, surname, age, sciences, password, email, phone_number)
+
+            # Add the new child to the database
+            db.session.add(new_child)
+            db.session.commit()
+
+            # Redirect to the child's dashboard
+            return redirect(url_for('child'))
+
+@app.route('/child')
 def child():
-    child_id = request.args.get("id")
-    # Извлекаем ребенка из базы данных, используя child_id
-    # Отрисовываем дочерний элемент, используя полученные данные
-    return render_template("child.hbs", child_id=child_id)
+    # Render the child's dashboard
+    return render_template('child.html')
 
 @app.route("/search", methods=["GET"])
 def search():
@@ -78,3 +93,6 @@ def search():
 # Поиск детей по предоставленным параметрам в базе данных
     # Отображение результатов поиска с помощью search.hbs
     return render_template("search.hbs")
+
+if __name__ == '__main__':
+    app.run()
