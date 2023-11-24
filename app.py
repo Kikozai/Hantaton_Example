d28b3d3665f04521bada8dee2273d10242696392
kikozai <djumvlad@yandex.ru>
2023-11-24 19:18:54 +0500

    pref: добавил поддержку сессий

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for,session
 from flask_sqlalchemy import SQLAlchemy
+import os 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:23675@localhost/child'  
+app.config['SECRET_KEY'] = os.urandom(24)  # Генирация ключа
 db = SQLAlchemy(app)
+
 
 # Модель ребёнка   
 class Child(db.Model):
@@ -14,7 +17,7 @@ class Child(db.Model):
     age = db.Column(db.Integer, nullable=False)
     sciences = db.Column(db.String(100))
     password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     phoneNumber = db.Column(db.String(20))
 
     def __init__(self, firstName, secondName, surname, age, sciences, password, email, phoneNumber):
@@ -31,26 +34,39 @@ class Child(db.Model):
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["GET"])
+# Роут для входа в аккаунт
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template('login.html', error=False)
+    elif request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-@app.route("/login", methods=["POST"])
-def login_post():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    error = request.form.get("error")
-    if email == "valid_email" and password == "valid_password":
-        return render_template("child.html")
-    else:
-        return render_template("login.html", email=email, password=password, error=True)
-# Register route
+        # Проверка, существует ли ребенок с указанным email и паролем
+        child = Child.query.filter_by(email=email, password=password).first()
+
+        if child:
+            # Сохранение ID ребенка в сессии
+            session['child_id'] = child.id
+            return redirect(url_for('child'))
+        else:
+            return render_template('login.html', error=True, email=email)
+
+# Роут для выхода из аккаунта
+@app.route('/logout')
+def logout():
+    # Удаление ID ребенка из сессии
+    session.pop('child_id', None)
+    return redirect(url_for('login'))
+
+# Роут для регистрации
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     elif request.method == 'POST':
-        # Get the registration data from the request
+        # Получение данных для регистрации из запроса
         first_name = request.form.get('firstName')
         second_name = request.form.get('secondName')
         surname = request.form.get('surname')
@@ -59,31 +75,35 @@ def register():
         sciences = request.form.getlist('sciences')
         email = request.form.get('email')
         phone_number = request.form.get('phoneNumber')
-
-        # Check if a child with the given email already exists
+           
+        # Проверка, существует ли ребенок с указанным email
         existing_child = Child.query.filter_by(email=email).first()
 
         if existing_child:
-            # Child with the given email already exists
+            # Ребенок с указанным email уже существует
             return render_template('register.html', error=True, firstName=first_name, secondName=second_name,
                                    surname=surname, password=password, age=age, sciences=sciences,
                                    email=email, phoneNumber=phone_number)
         else:
-            # Create a new child object
+            # Создание нового объекта ребенка
             new_child = Child(first_name, second_name, surname, age, sciences, password, email, phone_number)
 
-            # Add the new child to the database
+            # Добавление нового ребенка в базу данных
             db.session.add(new_child)
-            db.session.commit()
-
-            # Redirect to the child's dashboard
-            return redirect(url_for('child'))
-
-@app.route('/child')
+            db.session.commit()  # Сохранение изменений в базе данных
+            
+# Роут для страницы ребенка
+@app.route('/child', methods=['GET'])
 def child():
-    # Render the child's dashboard
-    return render_template('child.html')
-
+    # Проверка, есть ли ID ребенка в сессии
+    if 'child_id' in session:
+        child_id = session['child_id']
+        # Получение данных ребенка из базы данных
+        child = Child.query.get(child_id)
+        return render_template('child.html', child=child, id=child_id)
+    else:
+        # Если ID ребенка отсутствует, перенаправление на страницу входа в аккаунт
+        return redirect(url_for('login'))
 @app.route("/search", methods=["GET"])
 def search():
     firstName = request.args.get("firstName")
