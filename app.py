d4d461934a5352fec441e8c04bb916c4b7d17d39
David <david.ustich@yandex.ru>
2023-11-25 02:03:10 +0500

    чекни еще раз код

--- app.py
+++ app.py
@@ -96,23 +96,16 @@ def child():
 def search():
     if 'child_id' in session:
         id = session['child_id']
-        return redirect(url_for('search', id=id))
+    else:
+        id = None
 
     # Получаем параметры поиска из query запросов
     first_name = request.args.get('firstName')
     second_name = request.args.get('secondName')
     surname = request.args.get('surname')
     sciences = request.args.get('sciences', '').split(',')
-    min_age = int(request.args.get('minAge', 4))
-    max_age = int(request.args.get('maxAge', 25))
-
-    # Ограничение по возврасту: минимальный 4, максимальный 25
-    min_age = max(min_age, 4)
-    max_age = min(max_age, 25)
-
-    # Проверка на корректность ввода возраста
-    if min_age > max_age:
-        min_age, max_age = max_age, min_age
+    min_age = int(request.args.get('minAge') or 4)
+    max_age = int(request.args.get('maxAge') or 25)
 
     # Обработка логики поиска
     children = Child.query.filter(
@@ -125,7 +118,7 @@ def search():
 
     # Возвращаем выражение
     return render_template("search.html", firstName=first_name, secondName=second_name, surname=surname,
-                           sciences=', '.join(sciences), minAge=min_age, maxAge=max_age, children=children)
+                           sciences=', '.join(sciences), minAge=min_age, maxAge=max_age, children=children, id=id)
 
 @app.route("/achievement", methods=["POST"])
 def achievement():
