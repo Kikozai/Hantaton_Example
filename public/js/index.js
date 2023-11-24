7ea0b7a2da3803b2336708708488079d6f9c4918
David <david.ustich@yandex.ru>
2023-11-24 14:25:24 +0500

    delete public from repo

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelector('.links');
-
-if (localStorage.length != 0) {
-    links.innerHTML = `
-        <a href="/child?id=${localStorage.getItem('id')}" class="link">Личный кабинет</a>
-    `
-}
