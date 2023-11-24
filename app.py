62e04b8d9905adf90ed4a385167342fb89c933e2
kikozai <djumvlad@yandex.ru>
2023-11-24 14:13:19 +0500

    feat:the server has been rewritten and complies with the documentation

--- app.py
+++ app.py
@@ -5,92 +5,79 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:23675@localhost/users'
 db = SQLAlchemy(app)
 
+# Указываем путь к папке "public" для раздачи статики
+app.static_folder = 'public'
+
+# Модель ребёнка   
 class Child(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(255), nullable=False)
+    firstName = db.Column(db.String(50))
+    secondName = db.Column(db.String(50))
+    surname = db.Column(db.String(50))
+    password = db.Column(db.String(50))
     age = db.Column(db.Integer)
-    talent_area = db.Column(db.String(255))
-    contact_info = db.Column(db.String(255))
+    sciences = db.Column(db.String(200))
+    email = db.Column(db.String(100), unique=True)
+    phoneNumber = db.Column(db.String(20))
 
-# основная страница
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
-# функция добавления ребенка
-@app.route('/add_child', methods=['GET', 'POST'])
-def add_child():
-    if request.method == 'POST':
-        full_name = request.form['full_name']
-        age = request.form['age']
-        talent_area = request.form['talent_area']
-        contact_info = request.form['contact_info']
+    return render_template("index.html")
 
-        child = Child(full_name=full_name, age=age, talent_area=talent_area, contact_info=contact_info)
-        db.session.add(child)
-        db.session.commit()
-        return 'Child added successfully!'
-    return render_template('add_child.html')
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.hbs")
 
-# функция удаления ребенка
-@app.route('/delete_child', methods=['POST'])
-def delete_child():
-    child_id = request.form['child_id']
-    child = Child.query.get(child_id)
-    if child:
-        db.session.delete(child)
-        db.session.commit()
-        return 'Child deleted successfully!'
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    error = request.form.get("error")
+    if email == "valid_email" and password == "valid_password":
+        return render_template("child.hbs")
     else:
-        return 'Child not found!'
-    
-# функция поиска ребенка по имени
-@app.route('/search', methods=['GET', 'POST'])
+        return render_template("login.hbs", email=email, password=password, error=True)
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template("register.hbs")
+
+@app.route("/register", methods=["POST"])
+def register_post():
+    firstName = request.form.get("firstName")
+    secondName = request.form.get("secondName")
+    surname = request.form.get("surname")
+    password = request.form.get("password")
+    age = request.form.get("age")
+    sciences = request.form.getlist("sciences")
+    email = request.form.get("email")
+    phoneNumber = request.form.get("phoneNumber")
+    error = request.form.get("error")
+    existing_child = Child.query.filter_by(email=email).first()
+    if existing_child:
+        return render_template("register.hbs", firstName=firstName, secondName=secondName, surname=surname,
+                               password=password, age=age, sciences=sciences, email=email, phoneNumber=phoneNumber,
+                               error=True)
+    else:
+        new_child = Child(firstName=firstName, secondName=secondName, surname=surname, password=password, age=age,
+                          sciences=sciences, email=email, phoneNumber=phoneNumber)
+        db.session.add(new_child)
+        db.session.commit()
+        return render_template("child.hbs")
+
+@app.route("/child", methods=["GET"])
+def child():
+    child_id = request.args.get("id")
+    # Извлекаем ребенка из базы данных, используя child_id
+    # Отрисовываем дочерний элемент, используя полученные данные
+    return render_template("child.hbs", child_id=child_id)
+
+@app.route("/search", methods=["GET"])
 def search():
-    if request.method == 'POST':
-        full_name = request.form['full_name']
-        child = Child.query.filter_by(full_name=full_name).first()
-        if child:
-            return render_template('search_result.html', child=child)
-        else:
-            return 'Child not found!'
-    return render_template('search_child.html')
-
-# функция поиска ребенка по таланту
-@app.route('/search_talent', methods=['GET', 'POST'])
-def search_talent():
-    if request.method == 'POST':
-        talent_area = request.form['talent_area']
-        child = Child.query.filter_by(talent_area=talent_area).first()
-        if child:
-            return render_template('search_result.html', child=child)
-        else:
-            return 'Child not found!'
-    return render_template('search_child.html')
-
-# функция поиска ребенка по возрасту
-@app.route('/search_age', methods=['GET', 'POST'])
-def search_age():
-    if request.method == 'POST':
-        age = request.form['age']
-        children = Child.query.filter_by(age=age).all()
-        if children:
-            return render_template('search_result.html', children=children)
-        else:
-            return 'No children found!'
-    return render_template('search_child.html')
-
-# функция поиска ребенка по контактной информации
-@app.route('/search_contact', methods=['GET', 'POST'])
-def search_contact():
-    if request.method == 'POST':
-        contact_info = request.form['contact_info']
-        child = Child.query.filter_by(contact_info=contact_info).first()
-        if child:
-            return render_template('search_result.html', child=child)
-        else:
-            return 'Child not found!'
-    return render_template('search_child.html')
-
-
-if __name__ == '__main__':
-    app.run()
+    firstName = request.args.get("firstName")
+    secondName = request.args.get("secondName")
+    surname = request.args.get("surname")
+    sciences = request.args.get("sciences")   
+# Поиск детей по предоставленным параметрам в базе данных
+    # Отображение результатов поиска с помощью search.hbs
+    return render_template("search.hbs")
