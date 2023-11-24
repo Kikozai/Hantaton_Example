e68a76858b10575926d96f3bfce8e7eaa85ffb0b
David <david.ustich@yandex.ru>
2023-11-24 22:20:27 +0500

    финальная версия личного кабинета + правки в index.css

---
+++ static/js/child.js
@@ -0,0 +1,35 @@
+const sciences = document.querySelector('#sciences').innerHTML.split(', ');
+const scienceSelect = document.querySelector('#science-select');
+
+sciences.forEach(science => {
+    scienceSelect.innerHTML += `
+        <option value="${science}">${science}</option>
+    `;
+});
+
+const informationButton = document.querySelector('.full-information-button');
+const informationWindow = document.querySelector('.full-information-window');
+
+informationButton.addEventListener('click', () => {
+    informationWindow.classList.toggle('display-none');
+});
+
+const titleInput = document.querySelector('#title');
+const selects = Array.from(document.querySelectorAll('select'));
+const submitButton = document.querySelector('#add-button');
+
+function checkForm() {
+    if (
+        titleInput.value.length != 0 &&
+        selects.every(select => select.value != '')
+    ) {
+        submitButton.disabled = false
+    } else {
+        submitButton.disabled = true
+    }
+}
+
+titleInput.addEventListener('input', checkForm);
+selects.forEach(select => {
+    select.addEventListener('change', checkForm);
+});
